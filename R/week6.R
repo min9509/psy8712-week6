30a181d6d814b18278d9b328d16c2f6a65480601
minjeong seo <minjeongseo@infra04-wg012.eduroam.wireless.umn.edu>
2024-02-25 13:14:29 -0600

    Data Import - part 2-2 (Line 10~12)

--- R/week6.R
+++ R/week6.R
@@ -1,11 +1,15 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-install.packages("stringr")
 library(stringi)
 library(stringr)
 
 
+
 # Data Import
 citations <- stri_read_lines("data/citations.txt", encoding = "Windows-1252") # a  subset of "UTF-8" 
-?stri_read_lines
+citations_txt <- citations[!stri_isempty(citations)]
+length(citations) - length(citations_txt) # The number of blank lines eliminated
+mean(str_length(citations_txt)) # The average number of characters in each citation
+
+
 
