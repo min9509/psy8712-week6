9a683d8d91c8daa6ca2dac8af7b22bd63bf3b093
minjeong seo <minjeongseo@infra04-wg012.eduroam.wireless.umn.edu>
2024-02-25 14:06:07 -0600

    Data Cleaning - creates a new variable called perf_ref (Line 21)

--- R/week6.R
+++ R/week6.R
@@ -17,7 +17,8 @@ sample(citations_txt, 20)
 citations_tbl <- tibble(line = 1:length(citations_txt), cite = citations_txt) %>%
   mutate(cite = str_replace_all(cite, pattern = "[\"']", replacement = "")) %>%
   mutate(year = str_extract(cite, pattern = "\\b\\d{4}\\b")) %>%
-  mutate(page_start = str_match(string = cite, pattern = "([\\d]+)-") [,2])
+  mutate(page_start = str_match(cite, pattern = "([\\d]+)-") [,2]) %>%
+  mutate(perf_ref = str_detect(cite, pattern = regex("performance", ignore_case = TRUE)))
          
 citations_tbl
 
