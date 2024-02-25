fdf92b18bb252921ccfb1eecf65a101b2ae8c938
minjeong seo <minjeongseo@infra04-wg012.eduroam.wireless.umn.edu>
2024-02-25 13:08:38 -0600

    Data Import - Part 2-1 (Line 8 and 9)

--- R/week6.R
+++ R/week6.R
@@ -1,1 +1,11 @@
 # Script Settings and Resources
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+install.packages("stringr")
+library(stringi)
+library(stringr)
+
+
+# Data Import
+citations <- stri_read_lines("data/citations.txt", encoding = "Windows-1252") # a  subset of "UTF-8" 
+?stri_read_lines
+
